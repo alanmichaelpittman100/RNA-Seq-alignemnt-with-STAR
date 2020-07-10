cef716f37047b6e31c77e7a41c110585532cc4d1
Alan Michael Pittman <47422569+alanmichaelpittman100@users.noreply.github.com>
2020-07-10 16:35:58 +0100

    Update Generate_STAR_index.sh

--- Generate_STAR_index.sh
+++ Generate_STAR_index.sh
@@ -3,16 +3,12 @@
 ##############################################################
 
 
-
-
-
-
-/hades/Alan/RNA_seq_pipeline_AP/Resources/Software/STAR/STAR \
+./STAR \
 --runThreadN 8 \
 --runMode genomeGenerate \
---genomeDir /hades/Alan/RNA_seq_pipeline_AP/Resources/References/STAR_index \
---genomeFastaFiles /hades/Alan/RNA_seq_pipeline_AP/Resources/References/Genome_FASTA/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
---sjdbGTFfile /hades/Alan/RNA_seq_pipeline_AP/Resources/References/GTF/Homo_sapiens.GRCh38.91.gtf \
+--genomeDir /References/STAR_index \
+--genomeFastaFiles /References/Genome_FASTA/Homo_sapiens.GRCh38.dna.primary_assembly.fa \
+--sjdbGTFfile /References/GTF/Homo_sapiens.GRCh38.91.gtf \
 --sjdbOverhang 100
 
 
